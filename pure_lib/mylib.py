64ebcaaeb7a6c93b3712183980facd0608626afa
Corentin Le Molgat <corentinl@google.com>
2024-01-24 17:44:14 +0100

    update pure_lib sample

--- pure_lib/mylib.py
+++ pure_lib/mylib.py
@@ -1,15 +1,17 @@
-'''Plop module '''
+'''MyLib module '''
 
 import sys
 
-def free_function():
-    print(f'plop_function')
+def free_function(i=42):
+    print(f'free_function')
     print(f'version_info: {sys.version_info}')
-    return 42
+    return i
 
-class Plop:
+class MyLib:
     """A simple example class"""
-    i = 12345
 
-    def f(self):
-        return 31
+    def __init__(self, i=12345):
+        self.i = i
+
+    def method(self, i=0):
+        return i + self.i
