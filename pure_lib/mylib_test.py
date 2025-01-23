1121fa98b855f639e5448d20e53278cd2eb79802
Mizux Seiha <mizux.dev@gmail.com>
2025-01-23 23:57:03 +0100

    pure_lib: cleanup

--- pure_lib/mylib_test.py
+++ pure_lib/mylib_test.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-'''Test APIs'''
+"""Test APIs"""
 
 import sys
 import unittest
@@ -9,18 +9,20 @@ import pure_lib.mylib as mylib
 from pure_lib.mylib import MyLib
 
 if __debug__:
-    print(f'python path: {sys.path}')
+    print(f"python path: {sys.path}")
 
-    print(f'plop.python: ${dir(pure_lib.mylib)}')
+    print(f"plop.python: ${dir(pure_lib.mylib)}")
 
-    print(f'plop.python.mylib: ${dir(pure_lib.mylib)}')
-    print(f'mylib: ${dir(mylib)}')
+    print(f"plop.python.mylib: ${dir(pure_lib.mylib)}")
+    print(f"mylib: ${dir(mylib)}")
 
-    print(f'plop.python.mylib.MyLib: ${dir(pure_lib.mylib.MyLib)}')
-    print(f'mylib.MyLib: ${dir(mylib.MyLib)}')
+    print(f"plop.python.mylib.MyLib: ${dir(pure_lib.mylib.MyLib)}")
+    print(f"mylib.MyLib: ${dir(mylib.MyLib)}")
+
 
 class TestMyLib(unittest.TestCase):
-    '''Test MyLib'''
+    """Test MyLib"""
+
     def test_free_function(self):
         ret = mylib.free_function()
         self.assertEqual(42, ret)
@@ -29,5 +31,6 @@ class TestMyLib(unittest.TestCase):
         p = MyLib(31)
         self.assertEqual(31, p.method())
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main(verbosity=2)
