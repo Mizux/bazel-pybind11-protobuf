49046b6bd7bafdceac8ffe9d190a7a2e00982f71
Corentin Le Molgat <corentinl@google.com>
2025-10-15 14:50:27 +0200

    cleanup

--- cmake/cpp.cmake
+++ cmake/cpp.cmake
@@ -28,41 +28,6 @@ if(CHECK_TYPE)
   cmake_pop_check_state()
 endif()
 
-include(GNUInstallDirs)
-
-# add_cpp_test()
-# CMake function to generate and build C++ test.
-# Parameters:
-#  the C++ filename
-# e.g.:
-# add_cpp_test(foo.cpp)
-function(add_cpp_test FILE_NAME)
-  if(NOT BUILD_TESTING)
-    return()
-  endif()
-  message(STATUS "Configuring test ${FILE_NAME}: ...")
-  get_filename_component(TEST_NAME ${FILE_NAME} NAME_WE)
-  get_filename_component(COMPONENT_DIR ${FILE_NAME} DIRECTORY)
-  get_filename_component(COMPONENT_NAME ${COMPONENT_DIR} NAME)
-
-  if(APPLE)
-    set(CMAKE_INSTALL_RPATH
-      "@loader_path/../${CMAKE_INSTALL_LIBDIR};@loader_path")
-  elseif(UNIX)
-    set(CMAKE_INSTALL_RPATH "$ORIGIN/../${CMAKE_INSTALL_LIBDIR}:$ORIGIN")
-  endif()
-
-  add_executable(${TEST_NAME} ${FILE_NAME})
-  target_include_directories(${TEST_NAME} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
-  target_compile_features(${TEST_NAME} PRIVATE cxx_std_17)
-  target_link_libraries(${TEST_NAME} PRIVATE
-    GTest::gtest_main
-    ${PROJECT_NAMESPACE}::foo)
-
-  add_test(NAME cpp_${COMPONENT_NAME}_${TEST_NAME} COMMAND ${TEST_NAME})
-  message(STATUS "Configuring test ${FILE_NAME}: ...DONE")
-endfunction()
-
 # get_cpp_proto()
 # CMake macro to generate Protobuf cpp sources
 # Parameters:
@@ -105,9 +70,50 @@ macro(get_cpp_proto PROTO_HDRS PROTO_SRCS)
   endforeach()
 endmacro()
 
+################
+##  C++ Test  ##
+################
+# add_cpp_test()
+# CMake function to generate and build C++ test.
+# Parameters:
+#  the C++ filename
+# e.g.:
+# add_cpp_test(foo.cpp)
+function(add_cpp_test FILE_NAME)
+  if(NOT BUILD_TESTING)
+    return()
+  endif()
+  message(STATUS "Configuring test ${FILE_NAME}: ...")
+  get_filename_component(TEST_NAME ${FILE_NAME} NAME_WE)
+  get_filename_component(COMPONENT_DIR ${FILE_NAME} DIRECTORY)
+  get_filename_component(COMPONENT_NAME ${COMPONENT_DIR} NAME)
+
+  if(APPLE)
+    set(CMAKE_INSTALL_RPATH
+      "@loader_path/../${CMAKE_INSTALL_LIBDIR};@loader_path")
+  elseif(UNIX)
+    set(CMAKE_INSTALL_RPATH "$ORIGIN/../${CMAKE_INSTALL_LIBDIR}:$ORIGIN")
+  endif()
+
+  add_executable(${TEST_NAME} ${FILE_NAME})
+  target_include_directories(${TEST_NAME} PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
+  target_compile_features(${TEST_NAME} PRIVATE cxx_std_17)
+  target_link_libraries(${TEST_NAME} PRIVATE
+    GTest::gtest_main
+    ${PROJECT_NAMESPACE}::foo)
+
+  add_test(NAME cpp_${COMPONENT_NAME}_${TEST_NAME} COMMAND ${TEST_NAME})
+  message(STATUS "Configuring test ${FILE_NAME}: ...DONE")
+endfunction()
+
 ###################
 ## CMake Install ##
 ###################
+include(GNUInstallDirs)
+#include(GenerateExportHeader)
+#GENERATE_EXPORT_HEADER(${PROJECT_NAME})
+#install(FILES ${PROJECT_BINARY_DIR}/${PROJECT_NAME}_export.h
+#  DESTINATION ${CMAKE_INSTALL_INCLUDEDIR})
 install(EXPORT ${PROJECT_NAME}Targets
   NAMESPACE ${PROJECT_NAMESPACE}::
   DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}
